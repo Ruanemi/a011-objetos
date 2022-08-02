9733c3a430f1a148fb0ce80ff0326b16a9dfd5b8
Ruan <ruanemidio1412@gmail.com>
2022-08-02 14:20:22 -0300

    Exercício de objetos e arrays labenu

---
+++ exercicio1/script.js
@@ -0,0 +1,21 @@
+const sacolao = []
+const fruta1 = {
+    nome: 'laranja',
+    preco: 10.00,
+    disponibilidade:true
+
+}
+const fruta2 = {
+    nome: 'mamão',
+    preco: 9.00,
+    disponibilidade:true
+
+}
+const fruta3 = {
+    nome: 'pinha',
+    preco: 7.00,
+    disponibilidade:false
+
+}
+sacolao.push(fruta1, fruta2, fruta3)
+console.log(sacolao)
